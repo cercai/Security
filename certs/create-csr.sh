5cb5c1407a2bb47f32a82667caa6f267fbbc606b
ah31 <64860511+cercai@users.noreply.github.com>
2024-07-23 21:33:04 +0200

    Update create-csr.sh

--- certs/create-csr.sh
+++ certs/create-csr.sh
@@ -5,3 +5,6 @@ openssl req -new -nodes -keyout .private-key -out my.csr -config cert.cnf
 
 # READ CSR IN TEXT FORMAT
 openssl req -in my.csr -text -noout
+
+# READ CERTIFICATE IN TEXT FORMAT
+openssl x509 -in my.crt -text -noout
